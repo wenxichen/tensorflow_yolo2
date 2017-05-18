18aada7953128816aa6aacce2e083d2db4883806
WENXI CHEN <wenxichen@WENXIs-MacBook-Pro.local>
2017-05-18 21:33:49 +0800

    add script

---
+++ train_darknet19_imagenet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DATASET_DIR=/mnt/imagenetdisk/tf_imagenet
+TRAIN_DIR=/home/wenxichen/dl_yolo2/logs/darknet19_imagenet
+python imagenet_train.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_name=imagenet \
+    --dataset_split_name=train \
+    --dataset_dir=${DATASET_DIR}
